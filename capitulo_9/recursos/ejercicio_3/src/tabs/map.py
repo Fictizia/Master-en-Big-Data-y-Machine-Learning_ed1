e056f0cf802e863f0346f5eda13bb9570b926576
Moisés Martínez <momartinm@gmail.com>
2020-03-05 21:11:30 +0100

    Update map.py

--- capitulo_9/recursos/ejercicio_3/src/tabs/map.py
+++ capitulo_9/recursos/ejercicio_3/src/tabs/map.py
@@ -42,3 +42,82 @@ import pandas as pd
 import numpy as np
 
 def map_tab(data, states):
+
+	def make_plot(src, xs, ys):
+		
+		p = figure(plot_width = 1100, plot_height = 700, title = 'Map of 2018 Flights delays departing from NYC')
+		p.xaxis.visible = False
+		p.yaxis.visible = False
+		p.grid.visible = False
+		
+		patches_glyph = p.patches(xs, 
+					  ys, 
+					  fill_alpha=0.2, 
+					  fill_color = 'lightgray', 
+					  line_color="#884444", 
+					  line_width=2, 
+					  line_alpha=0.8)
+
+		lines_glyph = p.multi_line('flight_x', 
+					   'flight_y', 
+					   color = 'color', 
+					   line_width = 1, 
+					   line_alpha = 0.8, 
+					   hover_line_alpha = 1.0, 
+					   hover_line_color = 'color',
+					   legend = 'carrier', 
+					   source = src)
+
+		squares_glyph = p.square('origin_x_loc', 
+					 'origin_y_loc', 
+					 color = 'color', 
+					 size = 7, 
+					 source = src, 
+					 legend = 'avion')
+
+		circles_glyph = p.circle('dest_x_loc', 
+					 'dest_y_loc', 
+					 color = 'color', 
+					 size = 6, 
+					 source = src, 
+					 legend = 'avion')
+
+		p.renderers.append(patches_glyph)
+		p.renderers.append(lines_glyph)
+		p.renderers.append(squares_glyph)
+		p.renderers.append(circles_glyph)
+
+		hover_line = HoverTool(tooltips=[('Airline', '@carrier'),
+						 ('Number of Flights', '@count'),
+						 ('Average Delay', '@mean_delay{0.0}'),
+						 ('Max Delay', '@max_delay{0.0}'),
+						 ('Min Delay', '@min_delay{0.0}')],
+					line_policy = 'next',
+					renderers = [lines_glyph])
+		
+		hover_circle = HoverTool(tooltips=[('Origin', '@origin'),
+						   ('Dest', '@dest'),
+						   ('Distance (miles)', '@distance')],
+					 renderers = [circles_glyph])
+
+		p.legend.location = (10, 45)
+		p.add_tools(hover_line)
+		p.add_tools(hover_circle)
+
+		p = style(p) 
+		
+		return p
+	
+	def style(p):	
+		p.title.align = 'center'
+		p.title.text_font_size = '20pt'
+		p.title.text_font = 'serif'
+		p.xaxis.axis_label_text_font_size = '14pt'
+		p.xaxis.axis_label_text_font_style = 'bold'
+		p.yaxis.axis_label_text_font_size = '14pt'
+		p.yaxis.axis_label_text_font_style = 'bold'
+		p.xaxis.major_label_text_font_size = '12pt'
+		p.yaxis.major_label_text_font_size = '12pt'
+
+		return p
+		
